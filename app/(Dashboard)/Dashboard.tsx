4451a1787bdffa44de324e7d6889c1bc3a06eb93
Syed Abdullah <syedabdullah.developer@gmail.com>
2025-02-19 23:58:21 +0500

    removed deprecated responsive screen package

--- app/(Dashboard)/Dashboard.tsx
+++ app/(Dashboard)/Dashboard.tsx
@@ -4,6 +4,7 @@ import {
   TouchableOpacity,
   View,
   FlatList,
+  useWindowDimensions,
 } from "react-native";
 import { useContext, useState } from "react";
 import { Image } from "expo-image";
@@ -22,13 +23,10 @@ import third from "@/assets/images/SVG/dashboardpersonalgromming.svg";
 import fourth from "@/assets/images/SVG/thingstodo.svg";
 import fifth from "@/assets/images/SVG/recipe.svg";
 import { LinearGradient } from "expo-linear-gradient";
-import {
-  widthPercentageToDP as wp,
-  heightPercentageToDP as hp,
-} from "react-native-responsive-screen";
 
 const Dashboard = () => {
   const { userDetails } = useContext(ProductContext);
+  const { width, height } = useWindowDimensions();
 
   const cardDataArray = [
     {
@@ -159,6 +157,7 @@ const styles = StyleSheet.create({
     width: "100%",
     height: "100%",
     flex: 1,
+    // backgroundColor:"red"
   },
 
   container: {
@@ -342,7 +341,8 @@ const styles = StyleSheet.create({
   },
   todayProgress_card: {
     marginTop: 10,
-    height: hp("20%"),
+    // height: hp("20%"),
+    height: 300,
     width: "100%",
     backgroundColor: "#fff",
     justifyContent: "center",
--- components/Card.tsx
+++ components/Card.tsx
@@ -1,11 +1,18 @@
+
+
 import * as Progress from "react-native-progress";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
-import circle from "../assets/images/circle.png";
 import {
-  widthPercentageToDP as wp,
-  heightPercentageToDP as hp,
-} from "react-native-responsive-screen";
+  View,
+  Text,
+  TouchableOpacity,
+  useWindowDimensions,
+  StyleProp,
+  ViewStyle,
+  TextStyle,
+  ImageStyle,
+} from "react-native";
 import { Image } from "expo-image";
+import circle from "../assets/images/circle.png";
 import { COLORS, FontFamily, FontSize } from "../constants/theme";
 
 interface CardData {
@@ -25,10 +32,269 @@ interface CardComponentProps {
   data?: CardData;
 }
 
+const CardComponent: React.FC<CardComponentProps> = ({
+  onPress,
+  data = {},
+}) => {
+  const { width, height } = useWindowDimensions();
+
+  const {
+    title,
+    description,
+    items,
+    percentagetext,
+    progress = 0,
+    Picture,
+    bgColor,
+    badgeColor,
+    textcolor = "#626262",
+  } = data;
+
+  // Ensure progress is within the range [0, 1]
+  const clampedProgress = Math.max(0, Math.min(progress, 1));
+
+  return (
+    <TouchableOpacity
+      activeOpacity={0.8}
+      style={[
+        styles.cardContainer(width, height) as ViewStyle,
+        { backgroundColor: bgColor },
+      ]}
+      onPress={() => {
+        if (onPress) {
+          console.log("Card Pressed:", title);
+          onPress();
+        }
+      }}
+    >
+      <View style={styles.contentContainer(width, height)}>
+        <Text style={[styles.title(width), { color: textcolor }]}>{title}</Text>
+        <Text style={[styles.description(width, height), { color: textcolor }]}>
+          {description}
+        </Text>
+        <Text
+          style={[styles.badge(width, height), { backgroundColor: badgeColor }]}
+        >
+          {items}
+        </Text>
+        <Text style={[styles.percentage(width), { color: textcolor }]}>
+          {percentagetext}
+        </Text>
+        <View style={[styles.progressview(width), { borderColor: badgeColor }]}>
+          <Progress.Bar
+            progress={clampedProgress}
+            unfilledColor="white"
+            borderWidth={0}
+            color={badgeColor}
+            animated={true}
+            width={width * 0.45}
+            height={height * 0.005}
+          />
+        </View>
+      </View>
+      <View style={styles.contentContainer2(width)}>
+        <Image source={circle} style={styles.image2} />
+        <View style={styles.image}>
+          <Image
+            source={Picture}
+            style={styles.responsiveImage(width, height)}
+          />
+        </View>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+const styles = {
+  cardContainer: (width: number, height: number): ViewStyle => ({
+    height: height * 0.2,
+    width: "100%",
+    zIndex: 999,
+    borderRadius: width * 0.05,
+    flex: 1,
+    marginVertical: height * 0.015,
+    overflow: "hidden",
+    flexDirection: "row",
+  }),
+  contentContainer: (width: number, height: number): ViewStyle => ({
+    flexDirection: "column",
+    justifyContent: "center",
+    gap: height * 0.005,
+    alignItems: "flex-start",
+    flex: 1.2,
+    marginLeft: width * 0.06,
+  }),
+  contentContainer2: (width: number): ViewStyle => ({
+    position: "relative",
+    flex: 1,
+    padding: width * 0.02,
+  }),
+  title: (width: number): TextStyle => ({
+    fontFamily: FontFamily.H4_Regular,
+    fontSize: FontSize.itemtitle,
+    color: COLORS.black,
+    fontWeight: "600",
+    lineHeight: width * 0.055,
+    textAlign: "left",
+  }),
+  description: (width: number, height: number): TextStyle => ({
+    fontFamily: FontFamily.S1_Regular,
+    fontSize: FontSize.subtitle,
+    color: COLORS.gray,
+    fontWeight: "300",
+    marginVertical: height * 0.002,
+    textAlign: "left",
+  }),
+  badge: (width: number, height: number): TextStyle => ({
+    paddingHorizontal: width * 0.03,
+    paddingVertical: height * 0.005,
+    textAlign: "center",
+    borderRadius: width * 0.05,
+    fontSize: FontSize.subtitle,
+    fontFamily: FontFamily.subtitle,
+    color: "white",
+    marginVertical: height * 0.002,
+  }),
+  percentage: (width: number): TextStyle => ({
+    fontSize: width * 0.035,
+    color: "#000",
+  }),
+  progressview: (width: number): ViewStyle => ({
+    backgroundColor: "#FFFFFF",
+    borderWidth: 1,
+    borderRadius: width * 0.02,
+  }),
+  image: {
+    position: "absolute",
+    right: -16,
+    bottom: 0,
+  } as ViewStyle,
+  responsiveImage: (width: number, height: number): ImageStyle => ({
+    width: width * 0.4,
+    height: height * 0.15,
+    resizeMode: "contain",
+  }),
+  image2: {
+    position: "absolute",
+    right: 0,
+    top: -12,
+    width: "100%",
+    height: "100%",
+    resizeMode: "contain",
+  } as ImageStyle,
+};
+
+export default CardComponent;
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import * as Progress from "react-native-progress";
+// import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+// import circle from "../assets/images/circle.png";
+// import {
+//   widthPercentageToDP as wp,
+//   heightPercentageToDP as hp,
+// } from "react-native-responsive-screen";
+// import { Image } from "expo-image";
+// import { COLORS, FontFamily, FontSize } from "../constants/theme";
+
+// interface CardData {
+//   title?: string;
+//   description?: string;
+//   items?: string;
+//   percentagetext?: string;
+//   progress?: number;
+//   Picture?: any;
+//   bgColor?: string;
+//   badgeColor?: string;
+//   textcolor?: string;
+// }
+
+// interface CardComponentProps {
+//   onPress?: () => void;
+//   data?: CardData;
+// }
+
+// // const CardComponent: React.FC<CardComponentProps> = ({
+// //   onPress = () => {
+// //     console.log("clicked");
+// //   },
+// //   data = {},
+// // }) => {
+// //   const {
+// //     title,
+// //     description,
+// //     items,
+// //     percentagetext,
+// //     progress = 0,
+// //     Picture,
+// //     bgColor,
+// //     badgeColor,
+// //     textcolor = "#626262",
+// //   } = data;
+
+// //   // Ensure progress is within the range [0, 1]
+// //   const clampedProgress = Math.max(0, Math.min(progress, 1));
+
+// //   return (
+// //     // <TouchableOpacity
+// //     //   activeOpacity={1}
+// //     //   style={[styles.cardContainer, { backgroundColor: bgColor }]}
+// //     //   onPress={() => onPress()}
+// //     // >
+// //     <TouchableOpacity
+// //       activeOpacity={0.8}
+// //       style={[styles.cardContainer, { backgroundColor: bgColor }]}
+// //       onPress={() => {
+// //         console.log("Card Pressed:", title);
+// //         onPress();
+// //       }}
+// //     >
+// //       <View style={styles.contentContainer}>
+// //         <Text style={[styles.title, { color: textcolor }]}>{title}</Text>
+// //         <Text style={[styles.description, { color: textcolor }]}>
+// //           {description}
+// //         </Text>
+// //         <Text style={[styles.badge, { backgroundColor: badgeColor }]}>
+// //           {items}
+// //         </Text>
+// //         <Text style={[styles.percentage, { color: textcolor }]}>
+// //           {percentagetext}
+// //         </Text>
+// //         <View style={[styles.progressview, { borderColor: badgeColor }]}>
+// //           <Progress.Bar
+// //             progress={clampedProgress}
+// //             unfilledColor="white"
+// //             borderWidth={0}
+// //             color={badgeColor}
+// //             animated={true}
+// //             width={wp("45%")}
+// //             height={hp("0.5%")}
+// //           />
+// //         </View>
+// //       </View>
+// //       <View style={styles.contentContainer2}>
+// //         <Image source={circle} style={styles.image2} />
+// //         <View style={styles.image}>
+// //           <Image source={Picture} style={styles.responsiveImage} />
+// //         </View>
+// //       </View>
+// //     </TouchableOpacity>
+// //   );
+// // };
+
 // const CardComponent: React.FC<CardComponentProps> = ({
-//   onPress = () => {
-//     console.log("clicked");
-//   },
+//   onPress,
 //   data = {},
 // }) => {
 //   const {
@@ -47,17 +313,14 @@ interface CardComponentProps {
 //   const clampedProgress = Math.max(0, Math.min(progress, 1));
 
 //   return (
-//     // <TouchableOpacity
-//     //   activeOpacity={1}
-//     //   style={[styles.cardContainer, { backgroundColor: bgColor }]}
-//     //   onPress={() => onPress()}
-//     // >
 //     <TouchableOpacity
 //       activeOpacity={0.8}
 //       style={[styles.cardContainer, { backgroundColor: bgColor }]}
 //       onPress={() => {
-//         console.log("Card Pressed:", title);
-//         onPress();
+//         if (onPress) {
+//           console.log("Card Pressed:", title);
+//           onPress();
+//         }
 //       }}
 //     >
 //       <View style={styles.contentContainer}>
@@ -93,146 +356,83 @@ interface CardComponentProps {
 //   );
 // };
 
-const CardComponent: React.FC<CardComponentProps> = ({
-  onPress,
-  data = {},
-}) => {
-  const {
-    title,
-    description,
-    items,
-    percentagetext,
-    progress = 0,
-    Picture,
-    bgColor,
-    badgeColor,
-    textcolor = "#626262",
-  } = data;
+// const styles = StyleSheet.create({
+//   cardContainer: {
+//     height: hp("20%"),
+//     width: "100%",
+//     zIndex: 999,
+//     borderRadius: wp("5%"),
+//     flex: 1,
+//     marginVertical: hp("1.5%"),
+//     overflow: "hidden",
+//     flexDirection: "row",
+//   },
+//   contentContainer: {
+//     flexDirection: "column",
+//     justifyContent: "center",
+//     gap: hp("0.5%"),
+//     alignItems: "flex-start",
+//     flex: 1.2,
+//     marginLeft: wp("6%"),
+//   },
+//   contentContainer2: {
+//     position: "relative",
+//     flex: 1,
+//     padding: wp("2%"),
+//   },
+//   title: {
+//     fontFamily: FontFamily.H4_Regular,
+//     fontSize: FontSize.itemtitle,
+//     color: COLORS.black,
+//     fontWeight: "600",
+//     lineHeight: wp("5.5%"),
+//     textAlign: "left",
+//   },
+//   description: {
+//     fontFamily: FontFamily.S1_Regular,
+//     fontSize: FontSize.subtitle,
+//     color: COLORS.gray,
+//     fontWeight: "300",
+//     marginVertical: hp("0.2%"),
+//     textAlign: "left",
+//   },
+//   badge: {
+//     paddingHorizontal: wp("3%"),
+//     paddingVertical: hp("0.5%"),
+//     textAlign: "center",
+//     borderRadius: wp("5%"),
+//     fontSize: FontSize.subtitle,
+//     fontFamily: FontFamily.subtitle,
+//     color: "white",
+//     marginVertical: hp("0.2%"),
+//   },
+//   percentage: {
+//     fontSize: wp("3.5%"),
+//     color: "#000",
+//   },
+//   progressview: {
+//     backgroundColor: "#FFFFFF",
+//     borderWidth: 1,
+//     borderRadius: wp("2%"),
+//   },
+//   image: {
+//     position: "absolute",
+//     right: -16,
+//     bottom: 0,
+//   },
+//   responsiveImage: {
+//     width: wp("40%"),
+//     height: hp("15%"),
+//     resizeMode: "contain",
+//   },
+//   image2: {
+//     position: "absolute",
+//     right: 0,
+//     top: -12,
+//     width: "100%",
+//     height: "100%",
+//     resizeMode: "contain",
+//   },
+// });
 
-  // Ensure progress is within the range [0, 1]
-  const clampedProgress = Math.max(0, Math.min(progress, 1));
-
-  return (
-    <TouchableOpacity
-      activeOpacity={0.8}
-      style={[styles.cardContainer, { backgroundColor: bgColor }]}
-      onPress={() => {
-        if (onPress) {
-          console.log("Card Pressed:", title);
-          onPress();
-        }
-      }}
-    >
-      <View style={styles.contentContainer}>
-        <Text style={[styles.title, { color: textcolor }]}>{title}</Text>
-        <Text style={[styles.description, { color: textcolor }]}>
-          {description}
-        </Text>
-        <Text style={[styles.badge, { backgroundColor: badgeColor }]}>
-          {items}
-        </Text>
-        <Text style={[styles.percentage, { color: textcolor }]}>
-          {percentagetext}
-        </Text>
-        <View style={[styles.progressview, { borderColor: badgeColor }]}>
-          <Progress.Bar
-            progress={clampedProgress}
-            unfilledColor="white"
-            borderWidth={0}
-            color={badgeColor}
-            animated={true}
-            width={wp("45%")}
-            height={hp("0.5%")}
-          />
-        </View>
-      </View>
-      <View style={styles.contentContainer2}>
-        <Image source={circle} style={styles.image2} />
-        <View style={styles.image}>
-          <Image source={Picture} style={styles.responsiveImage} />
-        </View>
-      </View>
-    </TouchableOpacity>
-  );
-};
-
-const styles = StyleSheet.create({
-  cardContainer: {
-    height: hp("20%"),
-    width: "100%",
-    zIndex: 999,
-    borderRadius: wp("5%"),
-    flex: 1,
-    marginVertical: hp("1.5%"),
-    overflow: "hidden",
-    flexDirection: "row",
-  },
-  contentContainer: {
-    flexDirection: "column",
-    justifyContent: "center",
-    gap: hp("0.5%"),
-    alignItems: "flex-start",
-    flex: 1.2,
-    marginLeft: wp("6%"),
-  },
-  contentContainer2: {
-    position: "relative",
-    flex: 1,
-    padding: wp("2%"),
-  },
-  title: {
-    fontFamily: FontFamily.H4_Regular,
-    fontSize: FontSize.itemtitle,
-    color: COLORS.black,
-    fontWeight: "600",
-    lineHeight: wp("5.5%"),
-    textAlign: "left",
-  },
-  description: {
-    fontFamily: FontFamily.S1_Regular,
-    fontSize: FontSize.subtitle,
-    color: COLORS.gray,
-    fontWeight: "300",
-    marginVertical: hp("0.2%"),
-    textAlign: "left",
-  },
-  badge: {
-    paddingHorizontal: wp("3%"),
-    paddingVertical: hp("0.5%"),
-    textAlign: "center",
-    borderRadius: wp("5%"),
-    fontSize: FontSize.subtitle,
-    fontFamily: FontFamily.subtitle,
-    color: "white",
-    marginVertical: hp("0.2%"),
-  },
-  percentage: {
-    fontSize: wp("3.5%"),
-    color: "#000",
-  },
-  progressview: {
-    backgroundColor: "#FFFFFF",
-    borderWidth: 1,
-    borderRadius: wp("2%"),
-  },
-  image: {
-    position: "absolute",
-    right: -16,
-    bottom: 0,
-  },
-  responsiveImage: {
-    width: wp("40%"),
-    height: hp("15%"),
-    resizeMode: "contain",
-  },
-  image2: {
-    position: "absolute",
-    right: 0,
-    top: -12,
-    width: "100%",
-    height: "100%",
-    resizeMode: "contain",
-  },
-});
-
-export default CardComponent;
+// export default CardComponent;
