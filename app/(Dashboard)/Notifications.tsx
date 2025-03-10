8d43fb2e8b7e1533269b41a8e19a63bb20ccb87b
Syed Abdullah <syedabdullah.developer@gmail.com>
2025-03-10 12:36:16 +0500

    changes

--- app/(Dashboard)/Notifications.tsx
+++ app/(Dashboard)/Notifications.tsx
@@ -1,11 +1,19 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { COLORS } from "@/constants";
+import Apple from "../../assets/images/SwiftlistApp/Grocery/Fruits/Apple.svg"
 
+
+interface  svgProps {
+  width: number;
+  height: number;
+  src: number
+}
 const Notifications = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Notifications Coming Soon!</Text>
+      <Apple width={100} height={100}/>
     </View>
   );
 };
